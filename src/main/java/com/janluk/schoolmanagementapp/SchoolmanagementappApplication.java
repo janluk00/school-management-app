55bebcf570cd26a8df4aa2249ec155352d3e8d67
janluk00 <mrjankosz1@gmail.com>
2024-02-13 18:17:43 +0100

    initial commit + update .gitignore v2

---
+++ src/main/java/com/janluk/schoolmanagementapp/SchoolmanagementappApplication.java
@@ -0,0 +1,13 @@
+package com.janluk.schoolmanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolmanagementappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolmanagementappApplication.class, args);
+	}
+
+}
