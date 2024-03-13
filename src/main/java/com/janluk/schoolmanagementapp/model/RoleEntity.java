80464b749cbf3288b179618de7dc428b9c3d2c9e
janluk00 <mrjankosz1@gmail.com>
2024-03-13 21:02:10 +0100

    change role to a String type in RoleEntity

--- src/main/java/com/janluk/schoolmanagementapp/model/RoleEntity.java
+++ src/main/java/com/janluk/schoolmanagementapp/model/RoleEntity.java
@@ -1,6 +1,5 @@
 package com.janluk.schoolmanagementapp.model;
 
-import com.janluk.schoolmanagementapp.model.vo.RoleType;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -18,6 +17,5 @@ import java.io.Serializable;
 public class RoleEntity implements Serializable {
 
     @Id
-    @Enumerated(EnumType.STRING)
-    private RoleType role;
+    private String role;
 }
