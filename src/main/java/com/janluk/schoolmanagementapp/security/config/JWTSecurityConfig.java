d5ab1fa78a47151786ec20b4d21761e7af8070bf
janluk00 <mrjankosz1@gmail.com>
2024-08-18 21:22:53 +0200

    refactor: #19 getting rid of the omitted unused imports

--- src/main/java/com/janluk/schoolmanagementapp/security/config/JWTSecurityConfig.java
+++ src/main/java/com/janluk/schoolmanagementapp/security/config/JWTSecurityConfig.java
@@ -7,7 +7,6 @@ import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/janluk/schoolmanagementapp/security/config/SecurityConfig.java
+++ src/main/java/com/janluk/schoolmanagementapp/security/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.janluk.schoolmanagementapp.security.config;
 
 import com.janluk.schoolmanagementapp.security.service.CustomizedUserDetailsService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
